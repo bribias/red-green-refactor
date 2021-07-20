2efcd46b80a6c04de2514b255f15b1f453062c62
Brianna Bias <brianna.bias@gmail.com>
2021-07-20 09:36:43 -0700

    test for cap and filter written

---
+++ capitalize-and-filter.test.js
@@ -0,0 +1,11 @@
+import capitalizeAndFilter from './capitalize-and-filter';
+
+describe('capitalizeAndFilter', () => {
+  it('takes an array and filters strings that start with the letter f, capitalizing the rest', () => {
+    const strings = ['fire', 'flying high', 'nope', 'not today'];
+    const expected = ['NOPE', 'NOT TODAY'];
+    const actual = capitalizeAndFilter(strings);
+
+    expect(actual).toEqual(expected);
+  });
+});
