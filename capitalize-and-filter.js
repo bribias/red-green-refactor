997fef6905ee5f3b0d96c8fee11cdac15c2586f1
Brianna Bias <brianna.bias@gmail.com>
2021-07-20 10:18:05 -0700

    capitalize and filter not the one before whoops

--- capitalize-and-filter.js
+++ capitalize-and-filter.js
@@ -1,6 +1,6 @@
 const capitalizeAndFilter = (stringArr) => {
 
-  const filterArr = stringArr.filter(item => item[0] !== 'f');
+  const filterArr = stringArr.filter(item => item[0].toLowerCase() !== 'f');
   const result = filterArr.map(item => item.toUpperCase());
   return result;
 };
